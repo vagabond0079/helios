15e1ac8bd3a4e0b81d3724e7d68c231fe1ff05ad
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-26 17:37:21 -0700

    successfuly getting the users location

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -1,1 +1,11 @@
 'use strict';
+//This functions gets the current user's location
+function getLocation(){
+if(navigator.geolocation){
+   navigator.geolocation.getCurrentPosition(showData);
+}
+}
+function showData(position){
+  var currentLocation = [position.coords.latitude, position.coords.longitude].join();
+  console.log(currentLocation);
+}
