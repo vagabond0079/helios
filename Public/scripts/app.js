685f5135a56429141afe94fefb3c04e5c0ae052c
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-26 21:15:48 -0700

    created function to get current Date

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -9,3 +9,13 @@ function showData(position){
   var currentLocation = [position.coords.latitude, position.coords.longitude].join();
   console.log(currentLocation);
 }
+
+//This function gets the current users's Date
+function getDate(){
+  let setDate = new Date();
+  let setMonth = setDate.getMonth()
+  let setDay = setDate.getDate();
+  let setYear = setDate.getFullYear();
+
+  return (setMonth+1)+'/'+setDay+'/'+setYear;
+}
