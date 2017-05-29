d8b34c127368e0934181de8b4241c123304da19b
derekcfarmer <derek.farmer@gmail.com>
2017-05-29 15:03:05 -0700

    added secondsFromNoon and degreesFromNoon middleware

--- Public/scripts/middleware.js
+++ Public/scripts/middleware.js
@@ -1,22 +1,56 @@
 'use strict';
 
-//Returns total seconds in current day since midnight.
+//Takes input from the targetData object we receive from the USNO API along with the object property we're trying to translate (i.e., targetData.sundata or targetData.mooondata) as well as the index of the data (0, 1, etc) and returns a new Date object called targetDataDateObj in the format: Wed May 31 2017 01:21:00 GMT-0700 (PDT).
 
-function targetDataToDateObj(targetData, dataset, index) {
-  let targetDataDateObj = new Date(
+function targetDataToDateString(targetData, dataset, index) {
+  let targetDataDateString = new Date(
     targetData.year,
     targetData.month-1,
     targetData.day,
-    dataset[index].time.substring(0,2),
-    dataset[index].time.substring(3,5)
+    targetData[dataset][index].time.substring(0,2),
+    targetData[dataset][index].time.substring(3,5)
   );
-  return targetDataDateObj;
+  return targetDataDateString;
 };
 
-function currentTimeToSecondsFromMidnight(time) {
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-  let seconds = time.getSeconds();
+//Returns new array of objects with sun and moon data translated to date objects.
+
+function targetDataDateObj(targetData) {
+  let targetDataDateObj = {};
+  targetDataDateObj.sundataBC = targetDataToDateString(targetData, 'sundata', 0);
+  targetDataDateObj.sundataR = targetDataToDateString(targetData, 'sundata', 1);
+  targetDataDateObj.sundataU = targetDataToDateString(targetData, 'sundata', 2);
+  targetDataDateObj.sundataS = targetDataToDateString(targetData, 'sundata', 3);
+  targetDataDateObj.sundataEC = targetDataToDateString(targetData, 'sundata', 4);
+  targetDataDateObj.moondataS = targetDataToDateString(targetData, 'moondata', 0);
+  targetDataDateObj.moondataR = targetDataToDateString(targetData, 'moondata', 1);
+  targetDataDateObj.moondataU = targetDataToDateString(targetData, 'moondata', 2);
+  return targetDataDateObj;
+}
+
+//Returns total seconds in current day since midnight.
+
+function dateObjToSecondsFromMidnight(dateObj) {
+  let hours = dateObj.getHours();
+  let minutes = dateObj.getMinutes();
+  let seconds = dateObj.getSeconds();
   let totalSeconds = hours * 60 * 60 + minutes * 60 + seconds;
   return totalSeconds;
 };
+
+//Returns total seconds before or after Local Apparent Noon, known in our targetData as "phen: 'U' " (for Upper Transit). Time before noon is negative, time after noon is positive.
+
+function secondsFromNoon(targetData, targetTime) {
+  let minutesFromNoon =
+  (dateObjToSecondsFromMidnight(targetDataDateObj(targetData).sundataU) -
+  dateObjToSecondsFromMidnight(targetDataDateObj(targetData)[targetTime])) * -1;
+  return minutesFromNoon;
+};
+
+//Returns angular distance from noon (0 degrees). I.e., -126 degrees or 33 degrees.
+
+function degreesFromNoon(targetData, targetTime) {
+  let degreesFromNoon =
+  secondsFromNoon(targetData, targetTime) / 240;
+  return degreesFromNoon;
+}
