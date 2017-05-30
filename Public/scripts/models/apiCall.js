4e0ea4a618be62161125a586b7f7b820a325720c
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-30 15:45:49 -0700

    form is working, calling api with input values

--- Public/scripts/models/apiCall.js
+++ Public/scripts/models/apiCall.js
@@ -1,12 +1,28 @@
 'use strict';
+
+var currentLocation;
+var currentDate;
+
+/*these lines of code takes the values from the input field forms and sets up the currentDate and currentLocation by City*/
+$('#newData').on('submit', function(event){
+  event.preventDefault();
+  currentLocation = $('#city-Name').val();
+  currentDate = $('#date').val();
+  formatDate();
+});
+
+function formatDate(){
+  currentDate = currentDate.split('-');
+  currentDate = currentDate[1]+'/'+currentDate[2]+'/'+currentDate[0];
+  getDataByCityName();
+}
+
 //This functions gets the current user's location
 function getLocation(){
-
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(showData);
   }
 }
-var currentLocation;
 function showData(position){
   currentLocation = [position.coords.latitude, position.coords.longitude].join();
   console.log(currentLocation);
@@ -20,14 +36,20 @@ function getDate(){
   let setDay = setDate.getDate();
   let setYear = setDate.getFullYear();
 
-  return (setMonth+1)+'/'+setDay+'/'+setYear;
+  currentDate = (setMonth+1)+'/'+setDay+'/'+setYear;
+  return currentDate;
 }
 
 //variable/function to call the API
-var getData = function() { $.get( "http://api.usno.navy.mil/rstt/oneday?", {date:`${getDate()}`, coords:`${currentLocation}`,tz:'-7'})
+var getDataByCityName = function() { $.get( "http://api.usno.navy.mil/rstt/oneday?", {date:`${currentDate}`, loc:`${currentLocation}`,tz:'-7'})
   .done((data)=>
-    // console.log(data)
+  targetData = data
+  );};
+
+var getDataByCoordinates = function() { $.get( "http://api.usno.navy.mil/rstt/oneday?", {date:`${currentDate}`, coords:`${currentLocation}`,tz:'-7'})
+    .done((data)=>
   targetData = data
   );};
 
 getLocation();
+getDate();
