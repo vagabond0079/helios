2d1f9f61d56377c7f33f8e0df4390a7675025e10
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-30 09:57:30 -0700

    functions for current locations, current date, api call

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -13,9 +13,17 @@ function showData(position){
 //This function gets the current users's Date
 function getDate(){
   let setDate = new Date();
-  let setMonth = setDate.getMonth()
+  let setMonth = setDate.getMonth();
   let setDay = setDate.getDate();
   let setYear = setDate.getFullYear();
 
   return (setMonth+1)+'/'+setDay+'/'+setYear;
 }
+
+
+//variable/function to call the API
+var getData = function() { $.get( "http://api.usno.navy.mil/rstt/oneday?", {date:'1/5/2005', loc:'Renton,WA'})
+  .done(function( data ) {
+    console.table(data.sundata);
+  });};
+         
