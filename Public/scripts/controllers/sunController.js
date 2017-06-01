87dfa02b48b060865790549bc087bc779d5ee84c
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-01 15:08:03 -0700

    hamburger menu on screen, nav displaying block

--- Public/scripts/controllers/sunController.js
+++ Public/scripts/controllers/sunController.js
@@ -6,7 +6,5 @@ $('#start-button').click(function() {
   $('#loading-img').show();
   getDate();
   getLocation();
-  // getDataByCoordinates();
-  // app.Render.BC(targetData);
-  // app.sunView.translateBC(app.Render.angles[0].angle);
+
 });
