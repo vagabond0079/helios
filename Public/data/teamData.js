c74f2a20b5fe574fac2f41b5c63d363ef377a50d
Amanda Koster <amandakoster@gmail.com>
2017-06-01 17:26:47 -0700

    handlebars are working for team page - data is populating

---
+++ Public/data/teamData.js
@@ -0,0 +1,30 @@
+let teamData = [
+{
+imageUrl: "http://imgur.com/Nqs6Xaw",
+name: "Amanda Koster",
+bio: "SOME TEXT",
+github: "https://github.com/amandakoster",
+linkedIn: "https://www.linkedin.com/in/amandakoster/"
+},
+{
+imageUrl: "http://imgur.com/o4V9uQA",
+name: "Brian McFarlane",
+bio: "Hello I\'m Brian McFarlane. I\'m formaly a program manager at Microsoft. Working at Microsoft I discovered a passion for how internal tools and programs work. The best tools don\'t only benefit the end user, but also they also improve the quality of life of the agents helping our customers.",
+github: "https://github.com/Xanture",
+linkedIn: ""
+},
+{
+imageUrl: "http://imgur.com/s9BXVRe",
+name: "Derek Farmer",
+bio: "Derek is a 2013 graduate of Seattle University\'s School of Theology and Ministry. After receiving his Master of Pastoral Studies, Derek and his wife Katy spent a year in Rome where he was a Russell Berrie Fellow for Interreligious Studies at the Pontifical University of St. Thomas Aquinas, known as the Angelicum. Prior to graduate school, Derek was an intelligence officer in the US Navy and was stationed in the United Kingdom, South Korea, and aboard the USS Monterey (CG-61) homeported in Norfolk, Virginia. He was the Field Director and Volunteer Coordinator for Mike McGinn\'s 2009 campaign for Mayor of Seattle and spent two years working for at City Hall in constituent relations. Most recently, Derek was the Market Programs Director at Tilth Alliance where, among other programs, he managed the Food and Faith Initiative, which seeks to help faith communities build vegetable gardens on their properties in order to provide fresh produce for local food banks.",
+gitHub: "https://github.com/vagabond0079",
+linkedIn: "https://www.linkedin.com/in/derek-farmer-7a8a067a",
+},
+{
+imageUrl: "http://imgur.com/HnhPaxX",
+name: "Oscar Cauich",
+bio: "SOME TEXT",
+gitHub: "https://github.com/oscarcauich",
+linkedIn: "https://www.linkedin.com/in/oscar-cauich-3b055b9b/"
+}
+]
--- Public/scripts/controllers/sunController.js
+++ Public/scripts/controllers/sunController.js
@@ -6,7 +6,4 @@ $('#start-button').click(function() {
   $('#loading-img').show();
   getDate();
   getLocation();
-  // getDataByCoordinates();
-  // app.Render.BC(targetData);
-  // app.sunView.translateBC(app.Render.angles[0].angle);
 });
